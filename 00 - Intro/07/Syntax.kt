c2c232c3a7b4aba9162933dadfcaaabcaee7a5a5
James Gore <83005220+thejamesgore@users.noreply.github.com>
2024-08-15 00:19:36 +0100

    add: intro module code

---
+++ 00 - Intro/07/Syntax.kt
@@ -0,0 +1,11 @@
+fun main(){
+  // Organize the code below
+  println("Down by the bay")
+  println("Where the watermelons grow,")
+  println("Back to my home")
+  println("I dare not go.");
+  println("For if I do");
+  println("My mother will say")
+  println("Did you ever use an app that taught you how to snap?")
+  println("Down by the bay!")
+}
---
+++ 00 - Intro/08/Review.kt
@@ -0,0 +1,5 @@
+fun main(){
+  println(2024-1988)
+  println("Be tolerant with others and strict with yourself.") //Marcus Aurelius
+  print("Marcus Aurelius")
+}
