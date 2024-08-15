238a1806273f6a7c943a85859b33483106072190
James Gore <83005220+thejamesgore@users.noreply.github.com>
2024-08-15 23:41:45 +0100

    add: second module

---
+++ 01 - Data Types & Variables/00 - Date.kt
@@ -0,0 +1,11 @@
+fun main() {
+  // Write your code below ☀️
+  var todaysDate: String = "12/05/1988"
+
+  var currentWeather: String 
+
+  currentWeather = "cloudy"
+
+  println(todaysDate)
+  println(currentWeather)
+}
---
+++ 01 - Data Types & Variables/01 - Pi.kt
@@ -0,0 +1,4 @@
+fun main() {
+  // Write your code below
+  val pi : Double = 3.14
+}
