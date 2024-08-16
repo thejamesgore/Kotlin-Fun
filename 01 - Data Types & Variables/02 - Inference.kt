0c460a24493d9dfc01fbd36704a4bf685d5b2b3f
James Gore <83005220+thejamesgore@users.noreply.github.com>
2024-08-16 22:19:36 +0100

    add: second module

---
+++ 01 - Data Types & Variables/02 - Inference.kt
@@ -0,0 +1,14 @@
+fun main() {
+  val typeTest = "6"
+  /*
+    Declare your variable above ⬆️ from the following list and see the type:
+
+    "6"
+    6
+    true
+    "false"
+    2.6
+    
+   */ 
+  print("${typeTest::class.simpleName}")    
+}
